f589136e621438baa060562ffb5a88c8793ef51a
Mark Thomas <markdjthomas@gmail.com>
2018-02-20 11:49:56 -0400

    working on strava KM section

---
+++ beta2/strava/d3/js/progress_circle.js
@@ -0,0 +1,101 @@
+
+d3.csv('strava/data/recent_activities.csv', function(data){
+
+   var parent = d3.select('div.strava-elevation');
+   var parentWidth = parent.node().getBoundingClientRect().width * 0.95
+   var parentHeight = parent.node().getBoundingClientRect().height * 0.95
+   var color = '#90FFC4';
+   
+   var radius = (Math.min(parentWidth, parentHeight)) / 2;
+
+   var border = 5;
+   var padding = 10;
+   var startPercent = 0;
+
+   var totalElev = d3.sum(data.map(function(d){ return d.elevation}));
+   var endPercent = totalElev / 8848;
+
+   var twoPi = Math.PI * 2;
+   var formatPercent = d3.format('.0%');
+   var boxSize = (radius + padding) * 2;
+   var width = boxSize;
+   var height = boxSize;
+
+   var count = Math.abs((endPercent - startPercent) / 0.01);
+   var step = endPercent < startPercent ? - 0.01 : 0.01;
+
+   var arc = d3.arc()
+       .startAngle(0)
+       .innerRadius(radius)
+       .outerRadius(radius - border);
+
+   var svg = parent.append('svg')
+       .attr('width', width)
+       .attr('height', height);
+
+   var defs = svg.append('defs');
+
+   var filter = defs.append('filter')
+       .attr('id', 'blur');
+
+   filter.append('feGaussianBlur')
+       .attr('in', 'SourceGraphic')
+       .attr('stdDeviation', '4');
+
+   var g = svg.append('g')
+       .attr('class', 'everest-tracker')
+       .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+   var everestImg = g.append('image')
+       .attr("xlink:href", "img/003-mountain-1.svg")
+       .attr('width', boxSize / 2)
+       .attr('height', boxSize / 2)
+       .attr('x', -(boxSize/4))
+       .attr('y', -(boxSize/4 + 3*padding));
+
+   var meter = g.append('g')
+       .attr('class', 'progress-meter');
+
+   meter.append('path')
+       .attr('class', 'background')
+       .attr('fill', '#ccc')
+       .attr('fill-opacity', 0.5)
+       .attr('d', arc.endAngle(twoPi));
+
+   var foreground = meter.append('path')
+       .attr('class', 'foreground')
+       .attr('fill', color)
+       .attr('fill-opacity', 1)
+       .attr('stroke', color)
+       .attr('stroke-width', 5)
+       .attr('stroke-opacity', 1)
+       .attr('filter', 'url(#blur)');
+
+   var front = meter.append('path')
+       .attr('class', 'foreground')
+       .attr('fill', color)
+       .attr('fill-opacity', 1);
+
+   var numberText = g.append('text')
+       .attr('fill', '#343E5A')
+       .attr('text-anchor', 'middle')
+       .attr('dx', padding)
+       .attr('dy', radius - 4*padding);
+
+   function updateProgress(progress) {
+       foreground.attr('d', arc.endAngle(twoPi * progress));
+       front.attr('d', arc.endAngle(twoPi * progress));
+       numberText.text(formatPercent(progress));
+   }
+
+   var progress = startPercent;
+
+   (function loops() {
+       updateProgress(progress);
+       if (count > 0) {
+          count--;
+          progress += step;
+          setTimeout(loops, 10);
+       }
+   })();
+});
---
+++ beta2/strava/py/summarise_activities.py
@@ -0,0 +1,42 @@
+""" summarise_activities.py
+Builds a JSON representation of the last 30 days worth of activities for
+each member and includes their total number of runs and distance ran.
+
+Author: Mark Thomas
+Date: 2018-02-16
+"""
+
+## Imports
+import json
+import pandas as pd
+from datetime import datetime, timedelta
+from pymongo import MongoClient
+
+## Create the mongoDB client and specify the database
+client = MongoClient('mongodb://localhost:27017/')
+db = client.dartmouth_runners
+
+## Build a pandas DataFrame containing all of the DRA members id,
+## first name, last name, and medium profile image.
+cursor = db.members.find({}, {'id': 1, 'firstname': 1, 'profile': 1})
+members = pd.DataFrame(list(cursor)).drop('_id', axis=1)
+
+## Build a pandas DataFrame containing the total KMs and number
+## of runs for each member over the last 7 days of activities
+last_30d = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%SZ')
+cursor = db.activities.aggregate([
+    {'$match': {
+        'type': 'Run',
+        'start_date_local': { '$gte': last_30d }
+    }},
+    {'$group': {
+        '_id': '$athlete.id',
+        'distance': {'$sum': '$distance'},
+        'runs': {'$sum': 1},
+        'elevation': {'$sum': '$total_elevation_gain'}
+    }}])
+activities = pd.DataFrame(list(cursor)).rename(index=str, columns={'_id': 'id'})
+
+## Left join the two DataFrames by athlete 'id'
+member_data = pd.merge(members, activities, on='id')
+member_data.to_csv('strava/data/recent_activities.csv', index=False)
