f6b2a9494f7cff96830c23b753ef5039e0e167af
Mark Thomas <markdjthomas@gmail.com>
2018-08-14 10:21:33 -0300

    fixed spelling mistake in route description

---
+++ strava/update_route.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+set -e
+
+cd /Users/markdjthomas/Dropbox/coding/dartmouthrunners.github.io/
+
+/usr/local/bin/git pull
+/usr/local/bin/python3 strava/py/this_weeks_route.py
+/usr/local/bin/git add strava/data/route_details.json
+/usr/local/bin/git commit -m "updated route details"
+/usr/local/bin/git push https://markdjthomas:smb0%269QtZiN4Z%25k@github.com/dartmouthrunners/dartmouthrunners.github.io.git master
+
+echo "All done!"
