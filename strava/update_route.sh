72041fb7cb62d69108d3cef885b945e843504f2b
Mark Thomas <markdjthomas@gmail.com>
2018-10-02 10:03:23 -0300

    included a git add for this_weeks_route.py in the update script

--- strava/update_route.sh
+++ strava/update_route.sh
@@ -6,6 +6,7 @@ cd /Users/markdjthomas/Dropbox/coding/dartmouthrunners.github.io/
 /usr/local/bin/git pull
 /usr/local/Cellar/python/3.6.2/bin/python3 strava/py/this_weeks_route.py
 /usr/local/bin/git add strava/data/route_details.json
+/usr/local/bin/git add strava/py/this_weeks_route.py
 /usr/local/bin/git commit -m "updated route details"
 /usr/local/bin/git push https://markdjthomas:smb0%269QtZiN4Z%25k@github.com/dartmouthrunners/dartmouthrunners.github.io.git master
 
