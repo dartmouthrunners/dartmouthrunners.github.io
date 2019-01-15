62ee2e45385d8cd394d2e2a3387b8113b3c662cb
Mark Thomas <markdjthomas@gmail.com>
2019-01-15 13:53:32 -0400

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -8,11 +8,11 @@ Date: 2018-02-20
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 15809780
+ROUTE_ID = 16798191
 
-DESCRIPTION = "Hey crew! Join us this week as we head over the Circ, smell the smells of the Swiss Chalet on Main and head back via Waverly Road. <br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
+DESCRIPTION = "This week we're going to try out a new route! <br />We will start off pretty easy to warm up as we make our way from Sullivan's Pond to Portland and then back up Ochterloney. Once we hit Victoria Rd we'll start to engage those butt muscles by climbing Victoria, then Thistle, and finally Forest Road. <br />Once we've hit the summit, we'll make our way back to the pond via Lyngby and Crichton Aves. <br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
 
-RUN_DATE = "January 10, 2019"
+RUN_DATE = "January 17, 2019"
 
 # ===================================================== #
 
