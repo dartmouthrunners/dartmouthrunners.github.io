2b3550ff285d34be6481c07d9535c55e7a68e0a5
Mark Thomas <markdjthomas@gmail.com>
2019-03-12 13:15:49 -0300

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -8,11 +8,11 @@ Date: 2018-02-20
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 14936120
+ROUTE_ID = 15050887
 
-DESCRIPTION = "It's social night!<br />This week we'll run the usual social night route around Lake Banook and make our way to Brightwood Brewery for an after run beer.<br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
+DESCRIPTION = "Join us this week for our first run post Daylight Savings and run in the sun!<br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
 
-RUN_DATE = "March 7, 2019"
+RUN_DATE = "March 14, 2019"
 
 # ===================================================== #
 
