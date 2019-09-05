66c7807b1cc013632b88fe48299f3675cd390c14
Mark Thomas <markdjthomas@gmail.com>
2019-09-05 09:33:04 -0300

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -8,11 +8,11 @@ Date: 2018-02-20
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 14936214 
+ROUTE_ID = 14936120 
 
-DESCRIPTION = "Join us this week during the downpour and tackle Maple Street. We'll set off from the Gazebo, immediately go up Maple and then all the way down Slayter until Woodland Ave. We'll loop back via Victoria and then descend Thistle to Alderney and back to the Gazebo via Portland and Hawthorne.<br/> The forecast is looking pretty torrential this week, so dress accordingly!<br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
+DESCRIPTION = "It's social night! <br />Join us for a lap around Lake Banook and a post run beverage at the Wooden Monkey beer garden. <br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
 
-RUN_DATE = "August 29, 2019"
+RUN_DATE = "September 5, 2019"
 
 # ===================================================== #
 
