eefcf1ac9113e3ca4c58378094e9e619af946b86
Mark Thomas <markdjthomas@gmail.com>
2020-01-19 20:12:58 -0400

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -1,45 +1,51 @@
 # this_weeks_route.py
-#
 # Gets the most running route provided the route ID.
 #
 # @author: Mark Thomas
 # @date: 2018-02-20
 
-import sys
 import json
 import requests
+import argparse
 
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
 ROUTE_ID = 15809780
 
-DESCRIPTION = "This week we'll take to the streets of Westphal as we make out way down Prince Albert and over the highway.<br />Now that it is getting dark out early, don't forget to wear some brightly coloured clothes or light up gear.<br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm."
+DESCRIPTION = "This week we'll take to the streets of Westphal as we make out way down Prince Albert and over the highway.<br />Now that it is getting dark out early, don't forget to wear some brightly coloured clothes or light up gear.<br />As always, we meet at the Sullivans Pond Gazebo at 6:30pm." # noqa E501
 
 RUN_DATE = "January 16, 2020"
 
 # ===================================================== #
 
-# Import the auth_token from the system agruments
-auth_token = sys.argv[0]
-print(auth_token)
-print(type(auth_token))
+parser = argparse.ArgumentParser()
+parser.add_argument('--auth-token', type=str)
 
-# Get the route info from the Strava API
-header = {'Authorization': 'Bearer ' + auth_token}
-request_uri = 'https://www.strava.com/api/v3/routes/' + str(ROUTE_ID)
-response = requests.get(request_uri, headers=header)
 
-route_data = json.loads(response.content)
-reduced_json = {
-    'id': route_data['id'],
-    'name': route_data['name'],
-    'distance': route_data['distance'],
-    'elevation_gain': route_data['elevation_gain'],
-    'polyline': route_data['map']['polyline'],
-    'description': DESCRIPTION,
-    'date': RUN_DATE
-}
+def main():
+    # Parge the auth token
+    args = parser.parse_args()
 
-with open('strava/data/route_details.json', 'w') as f:
-    json.dump(reduced_json, f)
+    # Get the route info from the Strava API
+    header = {'Authorization': 'Bearer ' + args.auth_token}
+    request_uri = 'https://www.strava.com/api/v3/routes/' + str(ROUTE_ID)
+    response = requests.get(request_uri, headers=header)
+
+    route_data = json.loads(response.content)
+    reduced_json = {
+        'id': route_data['id'],
+        'name': route_data['name'],
+        'distance': route_data['distance'],
+        'elevation_gain': route_data['elevation_gain'],
+        'polyline': route_data['map']['polyline'],
+        'description': DESCRIPTION,
+        'date': RUN_DATE
+    }
+
+    with open('strava/data/route_details.json', 'w') as f:
+        json.dump(reduced_json, f)
+
+
+if __name__ == '__main__':
+    main()
