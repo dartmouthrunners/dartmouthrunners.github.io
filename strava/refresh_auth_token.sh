6fdbeb11bd8ed93ce77c3992175f199e8aefb8ee
Mark Thomas <markdjthomas@gmail.com>
2020-11-02 11:19:58 -0400

    updated the script for commiting new routes

---
+++ strava/refresh_auth_token.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+
+curl -X POST https://www.strava.com/api/v3/oauth/token \
+  -d client_id=23327 \
+  -d client_secret=894e5e0f2e02890f508926827edd33c7eaa09523 \
+  -d grant_type=refresh_token \
+  -d refresh_token=afe3595509c4286d0a57432683d47d7a147f1b87
--- strava/update_route.sh
+++ strava/update_route.sh
@@ -1,7 +1,6 @@
-#!/usr/local/bin/bash
-/usr/local/bin/git pull
-/usr/local/bin/python3 strava/py/this_weeks_route.py --auth-token $1
-/usr/local/bin/git add strava/data/route_details.json
-/usr/local/bin/git add strava/py/this_weeks_route.py
-/usr/local/bin/git commit -m "updated route details"
-/usr/local/bin/git push https://markdjthomas:smb0%269QtZiN4Z%25k@github.com/dartmouthrunners/dartmouthrunners.github.io.git master
+git pull
+python3 strava/py/this_weeks_route.py --auth-token $1
+git add strava/data/route_details.json
+git add strava/py/this_weeks_route.py
+git commit -m "updated route details"
+git push https://markdjthomas:smb0%269QtZiN4Z%25k@github.com/dartmouthrunners/dartmouthrunners.github.io.git master
