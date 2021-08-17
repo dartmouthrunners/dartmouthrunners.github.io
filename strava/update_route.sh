3ad745d3762deca14ba42e990b18edcf92eb57e6
markdjthomas <mark.thomas@jasco.com>
2021-08-17 09:40:19 -0300

    updated the script used to update the website

--- strava/update_route.sh
+++ strava/update_route.sh
@@ -5,4 +5,4 @@ python3 strava/py/this_weeks_route.py --auth-token $1
 git add strava/data/route_details.json
 git add strava/py/this_weeks_route.py
 git commit -m "updated route details"
-git push https://markdjthomas:smb0%269QtZiN4Z%25k@github.com/dartmouthrunners/dartmouthrunners.github.io.git master
+git push 
