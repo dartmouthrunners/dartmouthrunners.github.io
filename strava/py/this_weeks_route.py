9898c16b761310340468122f228144e584d0fab3
markdjthomas <mark.thomas@jasco.com>
2021-10-18 14:24:29 -0300

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -11,11 +11,11 @@ import argparse
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 2857640891464783166
+ROUTE_ID = 15809780
 
 DESCRIPTION = "" # noqa E501
 
-RUN_DATE = "October 14, 2021 @ 6:30PM"
+RUN_DATE = "October 21, 2021 @ 6:30PM"
 
 # ===================================================== #
 
