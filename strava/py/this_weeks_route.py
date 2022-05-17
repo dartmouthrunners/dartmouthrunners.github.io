f7f1d5cd8128669b226dd1f6434685799748d665
markdjthomas <markdjthomas@gmail.com>
2022-05-17 13:12:30 +0000

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -11,11 +11,11 @@ import argparse
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 15809780
+ROUTE_ID = 2961632850351101726
 
 DESCRIPTION = "" # noqa E501
 
-RUN_DATE = "May 10, 2022 @ 6:30PM"
+RUN_DATE = "May 17, 2022 @ 6:30PM"
 
 # ===================================================== #
 
