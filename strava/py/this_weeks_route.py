47a496a6923c8a91bb0467dde2a5e2d29d4f7ef4
Mark Thomas <markdjthomas@gmail.com>
2023-11-13 19:44:07 -0400

    updated route details

--- strava/py/this_weeks_route.py
+++ strava/py/this_weeks_route.py
@@ -11,11 +11,11 @@ import argparse
 # ===================================================== #
 # THIS IS THE ONLY THING THAT NEEDS TO CHANGE EACH WEEK #
 
-ROUTE_ID = 14936120
+ROUTE_ID = 3037509784214539140
 
 DESCRIPTION = "" # noqa E501
 
-RUN_DATE = "November 7, 2023 @ 6:30PM"
+RUN_DATE = "November 14, 2023 @ 6:30PM"
 
 # ===================================================== #
 
